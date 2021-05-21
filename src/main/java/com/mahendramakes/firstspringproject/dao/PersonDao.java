1c3db4558549f139bc89c86689ba090f392354cf
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-05-21 14:10:23 +0530

    Added all essential code for barebones project

---
+++ src/main/java/com/mahendramakes/firstspringproject/dao/PersonDao.java
@@ -0,0 +1,17 @@
+package com.mahendramakes.firstspringproject.dao;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.mahendramakes.firstspringproject.models.Person;
+
+public interface PersonDao {
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+    List<Person> selectAllPersons();
+}
